e14d3c9e48bd2ee01f8d37ad5e4f25cec5e11623
Niek Nijland <ngnijland@gmail.com>
2020-07-25 15:00:06 +0200

    fix: use media queries for condionally showing figcaptions

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import Img, { FluidObject } from "gatsby-image"
-import styled from "styled-components"
-
-import useWindowWidth from "../helpers/useWindowWidth"
+import styled, { css } from "styled-components"
 
 export enum CaptionPosition {
   Top,
@@ -25,7 +23,7 @@ const StyledImg = styled(Img)`
   border-radius: 1rem;
 `
 
-const Figcaption = styled.figcaption`
+const Figcaption = styled.figcaption<{ position: CaptionPosition }>`
   padding: 0 1rem;
 
   font-size: 0.875rem;
@@ -34,13 +32,30 @@ const Figcaption = styled.figcaption`
 `
 
 const FigcaptionTop = styled(Figcaption)`
+  display: none;
   margin-top: 0.5rem;
   margin-bottom: 0.75rem;
+
+  ${({ position }) =>
+    position === CaptionPosition.Top &&
+    css`
+      @media (min-width: 600px) {
+        display: block;
+      }
+    `}
 `
 
 const FigcaptionBottom = styled(Figcaption)`
   margin-top: 0.75rem;
   margin-bottom: 0.5rem;
+
+  ${({ position }) =>
+    position === CaptionPosition.Top &&
+    css`
+      @media (min-width: 600px) {
+        display: none;
+      }
+    `}
 `
 
 function Image(props: ImageProps): JSX.Element {
@@ -50,27 +65,17 @@ function Image(props: ImageProps): JSX.Element {
     captionPosition = CaptionPosition.Bottom,
     ...rest
   } = props
-  const [position, setPosition] = useState(captionPosition)
-  const width = useWindowWidth()
-
-  useEffect(() => {
-    if (captionPosition === CaptionPosition.Top && width) {
-      if (width < 600) {
-        setPosition(CaptionPosition.Bottom)
-      } else {
-        setPosition(CaptionPosition.Top)
-      }
-    }
-  }, [captionPosition, width])
 
   return (
     <Figure className={className}>
-      {caption && position === CaptionPosition.Top && (
-        <FigcaptionTop>{caption}</FigcaptionTop>
+      {caption && (
+        <FigcaptionTop position={captionPosition}>{caption}</FigcaptionTop>
       )}
       <StyledImg fadeIn={false} {...rest} />
-      {caption && position === CaptionPosition.Bottom && (
-        <FigcaptionBottom>{caption}</FigcaptionBottom>
+      {caption && (
+        <FigcaptionBottom position={captionPosition}>
+          {caption}
+        </FigcaptionBottom>
       )}
     </Figure>
   )
