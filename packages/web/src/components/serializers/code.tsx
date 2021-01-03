5c4b809a207a70dcced67e587414e72bf9920432
Niek Nijland <ngnijland@gmail.com>
2021-01-03 22:29:03 +0100

    style(web): syntax highlight code

--- packages/web/src/components/serializers/code.tsx
+++ packages/web/src/components/serializers/code.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export interface CodeProps {
   node: {
@@ -9,8 +11,15 @@ export interface CodeProps {
 
 export function Code({ node: { language, code } }: CodeProps): JSX.Element {
   return (
-    <pre data-language={language}>
-      <code>{code}</code>
-    </pre>
+    <SyntaxHighlighter
+      language={language === "html" ? "markdown" : language}
+      style={tomorrow}
+      customStyle={{
+        fontSize: "0.8em",
+        borderRadius: "1rem",
+      }}
+    >
+      {code}
+    </SyntaxHighlighter>
   );
 }
