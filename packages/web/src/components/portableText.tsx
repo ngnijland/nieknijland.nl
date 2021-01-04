d22eac971a41a0e868ca89f7c41c8eb4096df4e8
Niek Nijland <ngnijland@gmail.com>
2021-01-04 11:05:27 +0100

    style(web): inline code snippets

--- packages/web/src/components/portableText.tsx
+++ packages/web/src/components/portableText.tsx
@@ -12,39 +12,49 @@ const Container = styled.section`
   font-size: clamp(1.125rem, 3vw, 1.25rem);
   line-height: 1.5;
 
-  h1,
-  h2,
-  h3,
-  h4 {
+  > h1,
+  > h2,
+  > h3,
+  > h4 {
     line-height: 1.25em;
     margin: 0 0 0.5rem;
   }
 
-  h1 {
+  > h1 {
     font-size: 1.85em;
     margin-top: 2.5rem;
   }
 
-  h2 {
+  > h2 {
     font-size: 1.6em;
     margin-top: 2rem;
   }
 
-  h3 {
+  > h3 {
     font-size: 1.4em;
     margin-top: 1.5rem;
   }
 
-  h4 {
+  > h4 {
     font-size: 1.2em;
     margin-top: 1.25rem;
   }
 
-  blockquote {
+  > blockquote {
     margin: 1em 0 1em 1em;
 
     font-size: 1.25em;
     color: var(--text-color-secondary);
+  }
+
+  > p code {
+    padding: 0.1em 0.25rem;
+
+    font-family: Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace;
+    font-size: 0.8em;
+
+    background-color: #e5e5e5;
+    border-radius: 0.25rem;
   }
 `;
 
