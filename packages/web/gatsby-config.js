13299bcd0a365004932bfa549b7cdb99eadab5ad
ngnijland <ngnijland@gmail.com>
2024-01-21 20:28:35 +0100

    feat: add sitemap

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -2,17 +2,86 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+const siteUrl = "https://www.nieknijland.nl";
+
 module.exports = {
   siteMetadata: {
     title: `Niek Nijland`,
     description: `A web engineer andtravel enthousiast basedin The Netherlands.`,
-    url: "https://www.nieknijland.nl",
+    url: siteUrl,
     image: "/images/og-image.png",
     twitterUsername: "@ngnijland",
   },
   trailingSlash: `never`,
   plugins: [
     `gatsby-plugin-open-graph-images`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+          allSanityPost(sort: {publishedAt: ASC}) {
+            nodes {
+              _updatedAt
+              slug {
+                current
+              }
+              publishedAt
+            }
+          }
+        }
+        `,
+        resolveSiteUrl: () => siteUrl,
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+          allSanityPost: { nodes: allPosts },
+        }) => {
+          let blogUpdatedAt;
+
+          // Normalize blog post data
+          const posts = allPosts.reduce((acc, post) => {
+            /**
+             * Get the datetimestring of the last blog post
+             *
+             * publishedAt can be `null`, that means ordering the query on ASC sorts `null` to the end
+             */
+            if (post.publishedAt) {
+              blogUpdatedAt = post.publishedAt;
+            }
+
+            return {
+              ...acc,
+              [`/blog/${post.slug.current}`]: post._updatedAt,
+            };
+          }, {});
+
+          // When applicable add last updated datetime string to page
+          const pages = allPages.map((page) => {
+            // Get updatedAt datetime string for blog post detail pages from posts map
+            const updatedAt = posts[page.path];
+
+            if (updatedAt) {
+              return { ...page, updatedAt };
+            }
+
+            if (page.path === "/blog") {
+              return { ...page, updatedAt: blogUpdatedAt };
+            }
+
+            return page;
+          });
+
+          return pages;
+        },
+        excludes: ["/__generated/*"],
+        serialize: ({ path, updatedAt }) => ({ url: path, lastmod: updatedAt }),
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
