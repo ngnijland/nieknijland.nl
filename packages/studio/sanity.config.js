6ef0b4d45aa2c42df32cc844c6e3f8d51b8117fc
ngnijland <ngnijland@gmail.com>
2024-03-13 16:37:55 +0100

    refactor: setup dashboard and vision

--- packages/studio/sanity.config.js
+++ packages/studio/sanity.config.js
@@ -1,13 +1,72 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import schemas from "./schemas/schema";
 import { codeInput } from "@sanity/code-input";
+import { dashboardTool, projectInfoWidget } from "@sanity/dashboard";
+import { visionTool } from "@sanity/vision";
+import { documentListWidget } from "sanity-plugin-dashboard-widget-document-list";
+import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
+
+import schemas from "./schemas/schema";
+
+let sites = [];
+
+if (
+  process.env.SANITY_STUDIO_BUILD_HOOK_ID_STUDIO &&
+  process.env.SANITY_STUDIO_NAME_STUDIO &&
+  process.env.SANITY_STUDIO_API_ID_STUDIO
+) {
+  sites.push({
+    buildHookId: process.env.SANITY_STUDIO_BUILD_HOOK_ID_STUDIO,
+    title: "CMS",
+    name: process.env.SANITY_STUDIO_NAME_STUDIO,
+    apiId: process.env.SANITY_STUDIO_API_ID_STUDIO,
+  });
+}
+
+if (
+  process.env.SANITY_STUDIO_BUILD_HOOK_ID_WEB &&
+  process.env.SANITY_STUDIO_NAME_WEB &&
+  process.env.SANITY_STUDIO_API_ID_WEB
+) {
+  sites.push({
+    buildHookId: process.env.SANITY_STUDIO_BUILD_HOOK_ID_WEB,
+    title: "Website",
+    name: process.env.SANITY_STUDIO_NAME_WEB,
+    apiId: process.env.SANITY_STUDIO_API_ID_WEB,
+  });
+}
 
 export default defineConfig({
   title: "nieknijland.nl",
   projectId: "kq5iz0cu",
   dataset: "development",
-  plugins: [codeInput(), structureTool()],
+  plugins: [
+    codeInput(),
+    dashboardTool({
+      widgets: [
+        netlifyWidget({
+          title: "Netlify deploys",
+          sites,
+        }),
+        documentListWidget({
+          title: "Recent blog posts",
+          order: "_createdAt desc",
+          types: ["post"],
+          layout: { width: "medium", height: "large" },
+        }),
+        projectInfoWidget(),
+      ],
+    }),
+    structureTool(),
+    visionTool(),
+  ],
+  tools: (prev) => {
+    if (import.meta.env.DEV) {
+      return prev;
+    }
+
+    return prev.filter((tool) => tool.name !== "vision");
+  },
   schema: {
     types: schemas,
   },
